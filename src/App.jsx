6ffa6614960751654aa773bafa459afc2550fdd8
Meet Modasiya <modasiyameet1@gmail.com>
2025-01-08 23:34:28 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Component from "./components/NavBar";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import Component from "./components/NavBar";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <header className="w-full">
       <nav className="flex items-center w-full">
-        <div className="flex gap-5 w-full border border-red-400">
+        <div className="flex gap-5 w-full border border-red-400 text-red-500">
           <a href="">Home</a>
           <a href="">About</a>
           <a href="">Contact</a>
