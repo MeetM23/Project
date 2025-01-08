36b730a353b1dc1e5a942bec2948ee92f85f692a
HaiderL48 <haidelimdi@gmail.com>
2025-01-08 23:24:52 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Component from "./components/NavBar";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
 
+  let number = "hello";
   return (
     <>
-      <div>Hello</div>
+      <NavBar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header className="w-full">
+      <nav className="flex items-center w-full">
+        <div className="flex gap-5 w-full border border-red-400">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
